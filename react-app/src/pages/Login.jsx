ce5385762f4f7b652bb11dbd265a0c08888b5b9b
glsaacke <glsaacke@crimson.ua.edu>
2025-01-30 13:08:14 -0600

    login page styling done

--- react-app/src/pages/Login.jsx
+++ react-app/src/pages/Login.jsx
@@ -1,22 +1,24 @@
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 import '../styles/Login.css'
 
 const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
     return ( 
         <div className="login-container">
             <div className="home-overlay"></div>
             <div className="login-content">
-
                 <h1>LOGIN</h1>
-                <div>
-                    <p>USERNAME</p>
-
-                </div>
-                <div>
-                    <p>PASSWORD</p>
-
-                </div>
-                <button className='login-login'>LOGIN</button>
+                <form>
+                    <label>USERNAME</label>
+                    <input type="text" required value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    <label>PASSWORD</label>
+                    <input type="text" required value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <button className='login-login'>GO</button>
+                </form>
+                
                 <button className='login-create'>CREATE ACCOUNT</button>
 
             </div>
