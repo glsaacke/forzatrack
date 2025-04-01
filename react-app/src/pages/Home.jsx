fe03619e9c293d375f4086e4e1a18dcb194fdab3
glsaacke <143575030+glsaacke@users.noreply.github.com>
2025-04-01 18:25:24 -0500

    Home page revamp

--- react-app/src/pages/Home.jsx
+++ react-app/src/pages/Home.jsx
@@ -5,9 +5,9 @@ const Home = () => {
     return ( 
         <div className="home-container">
             <div className="home-overlay"></div>
-            <div className="home-content">
+            <div className="home-content container">
                 <h1>WELCOME TO FORZATRACK</h1>
-                <p>LOGIN OR SIGN UP TO VIEW YOUR DASHBOARD</p>
+                <h3>LOGIN OR SIGN UP TO VIEW YOUR DASHBOARD</h3>
                 <Link to='/login' className='home-login'>GET STARTED</Link>
             </div>
         </div>
