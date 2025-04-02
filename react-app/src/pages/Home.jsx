8682fa8681f0660515787f741277a98affd240cf
glsaacke <glsaacke@crimson.ua.edu>
2025-04-02 14:08:04 -0500

    about restyle and home animations

--- react-app/src/pages/Home.jsx
+++ react-app/src/pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     return ( 
         <div className="home-container">
             <div className="home-overlay"></div>
+            <div className="home-background"></div>
             <div className="home-content container">
                 <h1>WELCOME TO FORZATRACK</h1>
                 <h3>LOGIN OR SIGN UP TO VIEW YOUR DASHBOARD</h3>
