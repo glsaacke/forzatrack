e5a13800ee98a992e27c821b4dd2bbe89ddc694c
glsaacke <143575030+glsaacke@users.noreply.github.com>
2026-04-26 01:45:09 -0500

    analysis cards

--- react-app/src/components/AnalysisSection.jsx
+++ react-app/src/components/AnalysisSection.jsx
@@ -1,5 +1,8 @@
 import "../styles/AnalysisSection.css"
 import FastestCarCard from "./analysis/FastestCarCard";
+import FastestAvgCarCard from "./analysis/FastestAvgCarCard";
+import MostConsistentCarCard from "./analysis/MostConsistentCarCard";
+import MostUsedCarCard from "./analysis/MostUsedCarCard";
 
 const AnalysisSection = ({filteredRecords, cars}) => {
     return ( 
@@ -8,6 +11,9 @@ const AnalysisSection = ({filteredRecords, cars}) => {
             <div className="analysis-content">
                 <div className="top-cards">
                     <FastestCarCard filteredRecords={filteredRecords} cars={cars}/>
+                    <FastestAvgCarCard filteredRecords={filteredRecords} cars={cars}/>
+                    <MostConsistentCarCard filteredRecords={filteredRecords} cars={cars}/>
+                    <MostUsedCarCard filteredRecords={filteredRecords} cars={cars}/>
                 </div>
             </div>
         </div>
