ec1492ef29e2fbd5a84dd308a5bf963af85a38b5
Gavin Saacke <143575030+glsaacke@users.noreply.github.com>
2026-05-14 11:22:11 -0500

    Revert "input validation"

--- api/core/controllers/models/CarRequest.cs
+++ api/core/controllers/models/CarRequest.cs
@@ -1,20 +1,15 @@
-using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace api.core.controllers.models
 {
     public class CarRequest
     {
-        [Required]
-        [StringLength(100)]
         public string Make { get; set; }
-
-        [Required]
-        [StringLength(100)]
         public string Model { get; set; }
-
-        [Range(1886, 2100)]
         public int Year { get; set; }
-
         public int Deleted { get; set; } = 0;
     }
 }
--- api/core/controllers/models/UserRequest.cs
+++ api/core/controllers/models/UserRequest.cs
@@ -1,22 +1,15 @@
-using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace api.core.controllers.models
 {
     public class UserRequest
     {
-        [Required]
-        [StringLength(50, MinimumLength = 3)]
         public string Username { get; set; }
-
-        [Required]
-        [EmailAddress]
-        [StringLength(254)]
         public string Email { get; set; }
-
-        [Required]
-        [StringLength(100, MinimumLength = 8)]
         public string Password { get; set; }
-
         public int Deleted { get; set; } = 0;
     }
 }
