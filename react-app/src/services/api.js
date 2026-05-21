e328fd98861cfa985ccfcb1659333701911b1591
glsaacke <143575030+glsaacke@users.noreply.github.com>
2026-05-20 23:32:04 -0500

    remove log

--- react-app/src/services/api.js
+++ react-app/src/services/api.js
@@ -102,7 +102,6 @@ export async function createRecord(record){
 }
 
 export async function pingServer() {
-  console.log('API URL:', import.meta.env.VITE_API_URL)
   await fetch(`${BASE_URL}/health`);
 }
 
